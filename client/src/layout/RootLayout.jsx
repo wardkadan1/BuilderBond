a2937921d31ad1ac12f69fc66ee0e4582529a96f
Ward keadan <73485499+wardkadan1@users.noreply.github.com>
2025-05-10 00:38:44 +0300

    small fix

--- client/src/layout/RootLayout.jsx
+++ client/src/layout/RootLayout.jsx
@@ -59,6 +59,7 @@ export function RootLayout({
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
